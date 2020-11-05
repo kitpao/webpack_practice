4c09d031e5dad3085900da7f1bdcd558d0af6bff
kitpao <kitpao91@gmail.com>
2020-11-05 16:55:04 +0000

    new js and index files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash is required for next line
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
