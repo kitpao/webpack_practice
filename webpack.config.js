9bf36ea65e44c16cc2a721a8c32fd4ebe6dc4f74
kitpao <kitpao91@gmail.com>
2020-11-10 22:53:11 +0000

    delete previous loaders and pluggings

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,9 @@
 const path = require('path');
-const ConsoleLogOnBuildWebpackPlugin = require("./ConsoleLogOnBuildWebpackPlugin.js");
-const webpack  = require("webpack");
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.join(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      },
-      {
-        test: /\.jpeg$/,
-        use: [
-          {loader: "url-loader", options: {
-            limit: 10000
-          }},
-          {loader: "image-webpack-loader", options: {}}
-        ],
-      }
-    ]
-  },
-  plugins: [
-    new ConsoleLogOnBuildWebpackPlugin(),
-    new webpack.ContextReplacementPlugin()
-  ]
 };
