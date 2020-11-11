48228ab7d8f7ffeb16457f02deb185af072c450f
kitpao <kitpao91@gmail.com>
2020-11-11 16:37:55 -0600

    add files with different locations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import printMe from './print.js';
 // import {myName, yourName} from './myName';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash is required for next line
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  btn.innerHTML = 'Click me and check console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   // customized module
   //element.innerHTML = myName('Kit') + ' and ' + yourName('Pao');
