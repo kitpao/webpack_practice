ccdf1282239f8501cae5450500a85839fe4153ac
kitpao <kitpao91@gmail.com>
2020-11-11 16:30:13 -0600

    take out loaders from last branch about assets

--- src/index.js
+++ src/index.js
@@ -1,38 +1,17 @@
 import _ from 'lodash';
 import './style.css';
 // import {myName, yourName} from './myName';
-import Icon from './butterfly.jpeg';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
 
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
-//
 function component() {
   const element = document.createElement('div');
 
   // Lodash is required for next line
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
   // customized module
   //element.innerHTML = myName('Kit') + ' and ' + yourName('Pao');
 
   // add image
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
